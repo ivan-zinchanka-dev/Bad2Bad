de2b4a3a1607badb013db12c46f204dc364307bf
JanZinch <jan.zinchanka@mail.ru>
2023-10-05 15:27:51 +0300

    import: behaviour tree

---
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using NPBehave;
+
+namespace Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField] private HealthBody _healthBody;
+        
+        private Root _behaviorTree;
+
+
+        public bool IsAlive()
+        {
+            Debug.Log("IsAlive: " + (_healthBody.Health > 0));
+            
+            return _healthBody.Health > 0;
+        }
+
+        public void SelfDestroy()
+        {
+            Debug.Log("SelfDestroy");
+        }
+
+        void Start()
+        {
+
+            _behaviorTree = new Root(
+
+                new Selector(
+                    new Action(IsAlive),
+                    new Action(SelfDestroy)
+                    )
+                
+                
+                
+                
+                
+                );
+            
+            
+            _behaviorTree.Start();
+        }
+    }
+    
+    
+    
+}
